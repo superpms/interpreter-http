2362abbc12e4273cb7d361b1fa5d4f184359f245
Guest <kid666@88.com>
2025-11-03 00:17:46 +0800

    命名标准化

--- bin/const.php
+++ bin/const.php
@@ -3,22 +3,22 @@
 /**
  * 应用模式:多终端模式
  */
-const PMS_HTTP_APP_MODE_MULTIPLE = 'multiple';
+const HTTP_APP_MODE_MULTIPLE = 'multiple';
 
 /**
  * 应用模式:单终端端模式
  */
-const PMS_HTTP_APP_MODE_SINGLE = 'single';
+const HTTP_APP_MODE_SINGLE = 'single';
 
 /**
  * 路由模式:app模式
  */
-const PMS_HTTP_ROUTE_MODE_APP = 'app';
+const HTTP_ROUTE_MODE_APP = 'app';
 
 /**
  * 路由模式:终端模式
  */
-const PMS_HTTP_ROUTE_MODE_TERMINAL = 'terminal';
+const HTTP_ROUTE_MODE_TERMINAL = 'terminal';
 
 
 function HttpCustomErrorHandler($errno, $errstr, $errfile, int $errline){
